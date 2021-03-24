a06685b7bb0c80eefbd40c2f6f5ec90bf47b7f8d
Melvin Byrd <xb12mel@gmail.com>
2021-03-23 20:13:22 -0400

    made changes to the map

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,11 +1,11 @@
 // Create a map to be used as a base map
 function createMap(voting) {
-    var mymap = L.map('map-id').setView([35.501, -80.2442], 6.5);
+    var mymap = L.map('map-id').setView([35.10, -80.14], 6.6);
     
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
-        id: 'mapbox/streets-v11',
+        id: 'mapbox/outdoors-v11',
         accessToken: API_KEY
     }).addTo(mymap);
   }createMap()
