c2ba37335b769fe06283e9143d9ae67750ff1597
ajg2231 <43033837+ajg2231@users.noreply.github.com>
2021-03-29 21:31:45 -0400

    Update regression.js

--- static/js/regression.js
+++ static/js/regression.js
@@ -3648,9 +3648,9 @@ var push2 = ("The covariance of change in Republican voter registration and educ
 var push3 = [push1, push2];
 
 d3.select("ul")
-  .append("li")
   .data(push3)
   .enter()
+  .append("li")
   .text(function(d) {
     return(d)
 });
